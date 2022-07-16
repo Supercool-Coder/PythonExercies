1a2a2582936ea110bbd8fea644e17ee372b12c1f
supercool_5 <70787564+uttam-bhai@users.noreply.github.com>
2022-07-15 20:57:06 -0700

    Add files via upload

---
+++ 100 python codings questions with solution/Question 1.py
@@ -0,0 +1,16 @@
+# Question :-  Write a program to reverse an interger in python ?
+# Solution :-
+
+# Simple solution
+
+inter = [1,2,3,4,5]
+x = inter[::-1]
+print(x)
+
+y = input("Please enter anything that u wanna see in reverse:- ")
+u = y[::-1]
+print(u)
+
+# Medium solution ---- using loop
+num = int(input("Enter the number: "))
+
---
+++ 100 python codings questions with solution/Question 2.py
@@ -0,0 +1,2 @@
+# Question :- Write a program in python to check wheather an in teger is Armstrong number or not. ?
+# Solution :-
