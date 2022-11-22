2e7fbe9d94209b83a64a588375803356ac9d75a9
supercool_5 <70787564+Supercool-Coder@users.noreply.github.com>
2022-11-22 10:51:53 +0530

    Add files via upload

---
+++ Exercise/Exercise 5/Question28.py
@@ -0,0 +1,12 @@
+# Question 3 :- 
+# Check if the following variable:
+
+# flag = False
+
+# is an instance of the bool class and print the result to the console.
+# Expected result:
+# True
+
+# Solution : - 
+flag = False
+print(isinstance(flag , bool))
